46d78cb21bed8e933c2e6fa896a8aefe01ad3677
bibobba <alexvektov@gmail.com>
2026-09-21 01:24:32 +0300

    chore: add domain types

---
+++ src/types.ts
@@ -0,0 +1,3 @@
+export type EventStatus = 'draft'|'pending_teacher'|'rejected'|'approved'|'active'|'completed'|'cancelled';
+export type EventType = 'class'|'interclass'|'tournament'|'individual';
+export type ProposalStatus = 'pending'|'approved'|'rejected';
